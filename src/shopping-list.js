6838fe5185c86c8b67598450d43cfefd3d6206f9
Zoljargal Fallows <zol.fallows@hotmail.com>
2019-02-26 17:47:28 +0000

    added drill 3/4

--- src/shopping-list.js
+++ src/shopping-list.js
@@ -1,3 +1,4 @@
+'use strict';
 require('dotenv').config();
 const knex = require('knex');
 
@@ -10,8 +11,8 @@ function search(searchTerm){
   knex1('shopping_list')
     .select('*')
     .where({'name': searchTerm })
-    .then(data => console.log(data))
-};
+    .then(data => console.log(data));
+}
 
 function page(pageNumber){
   const offset = (pageNumber-1)* 6;
@@ -20,7 +21,26 @@ function page(pageNumber){
     .select('*')
     .limit(6)
     .offset(offset)
-    .then(data => console.log(data))
+    .then(data => console.log(data));
 }
 
-page(3);
+// page(3);
+
+function daysAgo(days){
+  knex1('shopping_list')
+    .select('*')
+    .where('date_added', '>', knex1.raw(`now() - '?? days'::INTERVAL`, days))
+    .then(data => console.log(data));
+}
+
+// daysAgo(7);
+
+function totalCost(){
+  knex1('shopping_list')
+    .select('category')
+    .sum('price')
+    .groupBy('category')
+    .then(data => console.log(data));
+}
+
+totalCost();
